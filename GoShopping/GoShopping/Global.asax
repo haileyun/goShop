a75102b9efc9a454e06769a0e96aaf8f578c7d32
haile <haile@DESKTOP-LSOATU1>
2018-06-08 00:34:54 +0800

    goshopping

---
+++ GoShopping/GoShopping/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoShopping.Global" Language="C#" %>
---
+++ GoShopping/GoShopping/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoShopping.Startup))]
+namespace GoShopping
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
